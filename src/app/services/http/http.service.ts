152c9a5be743c8da073d9a32ee97daa729f3439a
Adriano Anschau <adriano.anschau@deliverit.com.br>
2019-10-25 17:22:48 -0300

    Cria serviços cliente para requisições ajax

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,1 +1,12 @@
-export default class HttpClient {}
+import { of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map, catchError } from 'rxjs/operators';
+
+export default class HttpClient {
+    request(url = '') {
+        return ajax.get(url).pipe(
+            map(response => response.response),
+            catchError(of)
+        );
+    }
+}
