b1f5cdd79221cb1171f6455794ec13b7601a8fd0
Adriano Anschau <adrianoanschau@gmail.com>
2019-10-29 22:46:32 -0300

    Atualiza componente de listagem de dragões

--- src/app/containers/dragons-list/dragons-list.component.tsx
+++ src/app/containers/dragons-list/dragons-list.component.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import DragonsService from '../../services/dragons.service';
+
+import {Dragon} from "../../models/dragon.model";
+import {DragonItemComponent} from "../../components";
+import { DragonsService } from '../../services';
 import styles from './dragons-list.module.scss';
 
 type IProps = {};
@@ -9,16 +12,11 @@ type IState = {
     inEdit: string | null;
 }
 
-type Dragon = {
-    id: string;
-    name: string;
-}
-
 export default class DragonsListComponent extends React.Component<IProps, IState> {
 
     private service = new DragonsService();
 
-    constructor(props: IProps, context: any) {
+    constructor(props: Readonly<IProps>, context: any) {
         super(props, context);
         this.state = {
             dragons: [],
@@ -27,32 +25,56 @@ export default class DragonsListComponent extends React.Component<IProps, IState
     }
 
     componentDidMount(): void {
-        this.service.getDragons()
+        this.service.getDragons();
+        this.service.list$
             .subscribe((dragons: Dragon[]) => {
                 this.setState({ dragons });
             });
     }
 
-    editDragon(id: string) {
-        this.setState(({ inEdit }) => ({
-            inEdit: inEdit !== id ? id : null
-        }));
+    componentWillUnmount(): void {
+        this.service.list$.unsubscribe();
     }
 
-    listDragons() {
-        const { dragons, inEdit } = this.state;
-        return dragons.map(dragon =>
-            <li key={dragon.id}
-                className={inEdit === dragon.id ? styles.DragonEdit : ''}
-                onClick={() => this.editDragon(dragon.id)}>
-                {dragon.name}
-            </li>);
+    private onEdit(id: string | null) {
+        return () => this.setState({
+            inEdit: id
+        });
+    }
+
+    private onDelete(id: string | null) {
+        return () => {
+            if (id) {
+                this.service.delete(id);
+            }
+        }
+    }
+
+    private onSave(id: string | null) {
+        return (data: Dragon) => {
+            if (id) {
+                this.service.update(id, data);
+            }
+        }
     }
 
     render(): React.ReactElement<any> {
+        const { dragons, inEdit } = this.state;
         return (
             <div className={styles.DragonsList}>
-                <ul>{this.listDragons()}</ul>
+                <ul>
+                    {dragons.map(dragon =>
+                        <DragonItemComponent key={`${dragon.id}${dragon.name}${dragon.type}`}
+                                             route={`/dragon/${dragon.id}`}
+                                             dragon={dragon}
+                                             edit={dragon.id === inEdit}
+                                             onEdit={this.onEdit(dragon.id)}
+                                             onCancelEdit={this.onEdit(null)}
+                                             onDelete={this.onDelete(dragon.id)}
+                                             onSave={this.onSave(dragon.id)}
+                                        />
+                    )}
+                </ul>
             </div>
         );
     }
